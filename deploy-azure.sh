f5daa31de400db83520d7297f05122d06b0fb7c6
brianbacon <brian.bacon01@gmail.com>
2025-11-26 17:19:07 -0500

    updated build

--- deploy-azure.sh
+++ deploy-azure.sh
@@ -9,6 +9,14 @@ APP_NAME="pegs-jokers-app"
 LOCATION="East US 2"
 SKU="Free"
 
+# Load environment variables from .env.local
+if [ -f .env.local ]; then
+  echo "Loading environment variables from .env.local..."
+  export $(grep -v '^#' .env.local | xargs)
+else
+  echo "Warning: .env.local file not found!"
+fi
+
 echo "Creating resource group..."
 az group create --name $RESOURCE_GROUP --location "$LOCATION"
 
@@ -23,5 +31,20 @@ az staticwebapp create \
   --output-location "out" \
   --sku $SKU
 
+echo "Configuring environment variables..."
+az staticwebapp appsettings set \
+  --name $APP_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --setting-names \
+    NEXT_PUBLIC_FIREBASE_API_KEY="$NEXT_PUBLIC_FIREBASE_API_KEY" \
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="$NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN" \
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL="$NEXT_PUBLIC_FIREBASE_DATABASE_URL" \
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID="$NEXT_PUBLIC_FIREBASE_PROJECT_ID" \
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET="$NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET" \
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID="$NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID" \
+    NEXT_PUBLIC_FIREBASE_APP_ID="$NEXT_PUBLIC_FIREBASE_APP_ID"
+
 echo "Deployment initiated! Check Azure Portal for progress."
 echo "Your app will be available at: https://$APP_NAME.azurestaticapps.net"
+echo ""
+echo "Environment variables have been configured in Azure Static Web Apps."
